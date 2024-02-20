2aa240deda206041a51b62648efcf31e39fecf0f
Simone Carlo Surace <simsurace@gmx.net>
2024-02-20 21:07:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# ScalarKernelFunctions.jl
+This package implements kernel functions that are optimized for one-dimensional input and
+GPU-compatible.
+
+## Usage
+This package re-exports KernelFunctions.jl and uses the same interface. New kernels have the
+`Scalar` prefix, but have the same behavior on scalar input:
+```julia
+using ScalarKernelFunctions
+
+k1 = ScalarSEKernel() # from this package
+k2 = SEKernel() # from KernelFunctions.jl
+x = rand(100)
+kernelmatrix(k1, x) ≈ kernelmatrix(k2, x) # true
+```
+
+### GPU
+Easy:
+```julia
+using CUDA
+x = CUDA.rand(100)
+k = ScalarPeriodicKernel() |> gpu # ScalarPeriodicKernel{Float32}(1.0f0)
+kernelmatrix(k, x) # returns 100×100 CuArray{Float32, 2, CUDA.Mem.DeviceBuffer}
+```
+Omitting the `gpu` function will use `Float64`, which still works, but is much slower.
+
+## Supported Kernels
+- [x] `ScalarSEKernel`
+- [x] `ScalarLinearKernel`
+- [x] `ScalarPeriodicKernel`
+- [x] `ScalarKernelSum`
+- [x] `ScalarTransformedKernel`
+- [x] `ScalarScaledKernel`
+<!-- - [ ] `ScalarConstantKernel`
+- [ ] `WhiteKernel`
+- [ ] `EyeKernel`
+- [ ] `ZeroKernel`
+- [ ] `WienerKernel`
+- [ ] `CosineKernel`
+- [ ] `GaussianKernel`
+- [ ] `LaplacianKernel`
+- [ ] `ExponentialKernel`
+- [ ] `GammaExponentialKernel`
+- [ ] `ExponentiatedKernel`
+- [ ] `FBMKernel`
+- [ ] `MaternKernel`
+- [ ] `Matern12Kernel`
+- [ ] `Matern32Kernel`
+- [ ] `Matern52Kernel`
+- [ ] `PolynomialKernel`
+- [ ] `RationalKernel`
+- [ ] `RationalQuadraticKernel`
+- [ ] `GammaRationalKernel`
+- [ ] `PiecewisePolynomialKernel`
+- [ ] `NeuralNetworkKernel`
+- [ ] `KernelProduct`
+- [ ] `KernelTensorProduct`
+- [ ] `NormalizedKernel`
+- [ ] `GibbsKernel` -->
---
+++ src/ScalarKernelFunctions.jl
@@ -0,0 +1,132 @@
+module ScalarKernelFunctions
+
+import KernelFunctions: Kernel
+import KernelFunctions: kernelmatrix, kernelmatrix!, kernelmatrix_diag, kernelmatrix_diag!
+import KernelFunctions: Transform, IdentityTransform, with_lengthscale
+
+export Kernel
+export kernelmatrix, kernelmatrix!, kernelmatrix_diag, kernelmatrix_diag!
+export ScalarSEKernel, ScalarLinearKernel, ScalarPeriodicKernel
+export ScalarKernelSum, ScalarScaledKernel, with_lengthscale
+export TransformedScalarKernel, ScalarScaleTransform
+
+export gpu
+
+gpu(k::Kernel) = k
+gpu(t::Transform) = t
+gpu(x::Real) = Float32(x)
+
+abstract type ScalarKernel <: Kernel end
+
+function kernelmatrix(
+    k::ScalarKernel,
+    x::AbstractVector{<:Real},
+    y::AbstractVector{<:Real}
+)
+    return k.(x, y')
+end
+
+function kernelmatrix!(
+    K::AbstractMatrix{<:Real},
+    k::ScalarKernel,
+    x::AbstractVector{<:Real},
+    y::AbstractVector{<:Real}
+)
+    K .= k.(x, y')
+    return K
+end
+
+function kernelmatrix_diag(
+    k::ScalarKernel,
+    x::AbstractVector{<:Real},
+    y::AbstractVector{<:Real}
+)
+    return k.(x, y)
+end
+
+function kernelmatrix_diag!(
+    K::AbstractVector{<:Real},
+    k::ScalarKernel,
+    x::AbstractVector{<:Real},
+    y::AbstractVector{<:Real}
+)
+    K .= k.(x, y)
+    return K
+end
+
+
+
+struct ScalarSEKernel <: ScalarKernel end
+(k::ScalarSEKernel)(x, y) = exp(-abs2(x - y) / 2)
+
+struct ScalarLinearKernel <: ScalarKernel end
+(k::ScalarLinearKernel)(x, y) = x * y
+
+struct ScalarPeriodicKernel{T<:Real} <: ScalarKernel
+    r::T
+end
+ScalarPeriodicKernel() = ScalarPeriodicKernel(1.)
+(k::ScalarPeriodicKernel)(x, y) = exp(-abs2(sinpi(x - y) / k.r) / 2)
+gpu(k::ScalarPeriodicKernel) = ScalarPeriodicKernel(gpu(k.r))
+
+
+
+struct ScalarKernelSum{T1<:Kernel, T2<:Kernel} <: ScalarKernel
+    k1::T1
+    k2::T2
+end
+
+ScalarKernelSum(kernels::Tuple) = ScalarKernelSum(kernels...)
+
+function ScalarKernelSum(k1::Kernel, k2::Kernel, k3::Kernel, ks...)
+    return ScalarKernelSum(k1, ScalarKernelSum(k2, k3, ks...))
+end
+
+(k::ScalarKernelSum)(x, y) = k.k1(x, y) + k.k2(x, y)
+gpu(k::ScalarKernelSum) = ScalarKernelSum(gpu(k.k1), gpu(k.k2))
+Base.:+(k1::ScalarKernel, k2::ScalarKernel) = ScalarKernelSum(k1, k2)
+
+
+
+struct ScalarScaledKernel{Tk<:Kernel,Tσ²<:Real} <: ScalarKernel
+    kernel::Tk
+    σ²::Tσ²
+end
+
+(k::ScalarScaledKernel)(x, y) = k.σ² * k.kernel(x, y)
+gpu(k::ScalarScaledKernel) = ScalarScaledKernel(gpu(k.kernel), gpu(k.σ²))
+Base.:*(w::Real, k::ScalarKernel) = ScalarScaledKernel(k, w)
+
+
+
+struct TransformedScalarKernel{Tk<:Kernel,Tr<:Transform} <: ScalarKernel
+    kernel::Tk
+    transform::Tr
+end
+
+(k::TransformedScalarKernel)(x, y) = k.kernel(k.transform(x), k.transform(y))
+
+function gpu(k::TransformedScalarKernel)
+    return TransformedScalarKernel(gpu(k.kernel), gpu(k.transform))
+end
+
+Base.:∘(k::ScalarKernel, t::Transform) = TransformedScalarKernel(k, t)
+
+function Base.:∘(k::TransformedScalarKernel, t::Transform)
+    return TransformedScalarKernel(k.kernel, k.transform ∘ t)
+end
+
+Base.:∘(k::TransformedScalarKernel, ::IdentityTransform) = k
+
+
+
+struct ScalarScaleTransform{T<:Real} <: Transform
+    s::T
+end
+
+(t::ScalarScaleTransform)(x) = t.s * x
+Base.:∘(t::ScalarScaleTransform, u::ScalarScaleTransform) = ScalarScaleTransform(t.s * u.s)
+gpu(t::ScalarScaleTransform) = ScalarScaleTransform(gpu(t.s))
+with_lengthscale(k::ScalarKernel, l::Real) = k ∘ ScalarScaleTransform(inv(l))
+
+end # module ScalarKernelFunctions
